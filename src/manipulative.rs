212fd0dac6a13de53acb83011013fb495dd4eea4
bababax11 <bababax11@example.com>
2020-01-31 23:16:01 +0900

    assign for manipulative

--- src/manipulative.rs
+++ src/manipulative.rs
@@ -1,111 +1,98 @@
 use std::ops::{Add, Div, Mul, Neg, Sub};
+use std::ops::{AddAssign, DivAssign, MulAssign, SubAssign};
 #[derive(Clone, Debug, PartialEq)]
 struct Manipulative<T>
 where
-    T: Copy
-        + Add<Output = T>
-        + Sub<Output = T>
-        + Neg<Output = T>
-        + Mul<Output = T>
-        + Div<Output = T>,
+    T: Copy,
 {
     factors: Vec<T>,
 }
 impl<T> Manipulative<T>
 where
-    T: Copy
-        + Add<Output = T>
-        + Sub<Output = T>
-        + Neg<Output = T>
-        + Mul<Output = T>
-        + Div<Output = T>,
+    T: Copy + AddAssign + SubAssign + Neg<Output = T> + MulAssign + DivAssign,
 {
     pub fn new(factors: Vec<T>) -> Self {
         Self { factors: factors }
     }
 }
+impl<T> AddAssign for Manipulative<T>
+where
+    T: Copy + AddAssign,
+{
+    fn add_assign(&mut self, other: Manipulative<T>) {
+        let min_deg;
+        let max_deg;
+        let is_self_shorter;
+        if self.factors.len() < other.factors.len() {
+            min_deg = self.factors.len();
+            max_deg = other.factors.len();
+            is_self_shorter = true;
+        } else {
+            min_deg = other.factors.len();
+            max_deg = self.factors.len();
+            is_self_shorter = false;
+        }
+        for (l, r) in self.factors.iter_mut().zip(other.factors.iter()) {
+            *l += *r;
+        }
+        if is_self_shorter {
+            for i in min_deg..max_deg {
+                self.factors
+                    .push(unsafe { *other.factors.get_unchecked(i) })
+            }
+        }
+    }
+}
+impl<T> SubAssign for Manipulative<T>
+where
+    T: Copy + SubAssign + Neg<Output = T>,
+{
+    fn sub_assign(&mut self, other: Manipulative<T>) {
+        let min_deg;
+        let max_deg;
+        let is_self_shorter;
+        if self.factors.len() < other.factors.len() {
+            min_deg = self.factors.len();
+            max_deg = other.factors.len();
+            is_self_shorter = true;
+        } else {
+            min_deg = other.factors.len();
+            max_deg = self.factors.len();
+            is_self_shorter = false;
+        }
+        for (l, r) in self.factors.iter_mut().zip(other.factors.iter()) {
+            *l -= *r;
+        }
+        if is_self_shorter {
+            for i in min_deg..max_deg {
+                self.factors
+                    .push(-unsafe { *other.factors.get_unchecked(i) })
+            }
+        }
+    }
+}
 impl<T> Add for Manipulative<T>
 where
-    T: Copy
-        + Add<Output = T>
-        + Sub<Output = T>
-        + Neg<Output = T>
-        + Mul<Output = T>
-        + Div<Output = T>,
+    T: Copy + AddAssign,
 {
     type Output = Manipulative<T>;
 
     fn add(self, other: Manipulative<T>) -> Self {
-        let min_deg;
-        let max_deg;
-        let is_self_shorter;
-        if self.factors.len() < other.factors.len() {
-            min_deg = self.factors.len();
-            max_deg = other.factors.len();
-            is_self_shorter = true;
-        } else {
-            min_deg = other.factors.len();
-            max_deg = self.factors.len();
-            is_self_shorter = false;
-        }
-        let mut new_factors = Vec::with_capacity(max_deg);
-        for (l, r) in self.factors.iter().zip(other.factors.iter()) {
-            new_factors.push(l.clone() + r.clone());
-        }
-        if is_self_shorter {
-            for i in min_deg..max_deg {
-                new_factors.push(unsafe { *other.factors.get_unchecked(i) })
-            }
-        } else {
-            for i in min_deg..max_deg {
-                new_factors.push(unsafe { *self.factors.get_unchecked(i) })
-            }
-        }
-        Self {
-            factors: new_factors,
-        }
+        let mut result = self.clone();
+        result += other;
+        result
     }
 }
 impl<T> Sub for Manipulative<T>
 where
-    T: Copy
-        + Add<Output = T>
-        + Sub<Output = T>
-        + Neg<Output = T>
-        + Mul<Output = T>
-        + Div<Output = T>,
+    T: Copy + SubAssign + Neg<Output = T>,
 {
     type Output = Manipulative<T>;
 
     fn sub(self, other: Manipulative<T>) -> Self {
-        let min_deg;
-        let max_deg;
-        let is_self_shorter;
-        if self.factors.len() < other.factors.len() {
-            min_deg = self.factors.len();
-            max_deg = other.factors.len();
-            is_self_shorter = true;
-        } else {
-            min_deg = other.factors.len();
-            max_deg = self.factors.len();
-            is_self_shorter = false;
-        }
-        let mut new_factors = Vec::with_capacity(max_deg);
-        for (l, r) in self.factors.iter().zip(other.factors.iter()) {
-            new_factors.push(l.clone() - r.clone());
-        }
-        if is_self_shorter {
-            for i in min_deg..max_deg {
-                new_factors.push(- unsafe { *other.factors.get_unchecked(i) })
-            }
-        } else {
-            for i in min_deg..max_deg {
-                new_factors.push(unsafe { *self.factors.get_unchecked(i) })
-            }
-        }
-        Self {
-            factors: new_factors,
-        }
+        let mut result = self.clone();
+        result -= other;
+        result
     }
 }
 
