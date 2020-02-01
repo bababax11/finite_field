c2d0305d8b7bee2fde42f636e41138741c87b7c3
Odagawa <xtctl.1234@gmail.com>
2020-02-01 22:32:39 +0900

    コメントの追記

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use finite_field::decryption::euclid_decrypt;
 use finite_field::manipulative::Manipulative;
 use finite_field::modulo::Field;
 fn main() {
-    let x = Field::new(2, 4);
-    println!("{:?}", x * Field::new(2, 4));
+    // 整式の割り算
     let a = Manipulative::new(vec![1, 2, 1]);
     let b = Manipulative::new(vec![1, 1]);
     assert_eq!(
@@ -14,6 +13,8 @@ fn main() {
             Manipulative::new(vec![0, 0, 0])
         )
     );
+
+    // 過去問の問題のユークリッド復号
     let w = euclid_decrypt(11, 7, 5, 2, &[1, -1, 1, 0, 3, 2, 0, 1]);
     println!("{:?}", w);
     assert_eq!(
